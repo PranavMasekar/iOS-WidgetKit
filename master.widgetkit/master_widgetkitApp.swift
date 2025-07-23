58e8b5f32558577491ea94407b0eb197fa0a81c3
PranavMasekar <pranavmasekar4@gmail.com>
2025-07-23 10:32:28 +0530

    Initial Commit

---
+++ master.widgetkit/master_widgetkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  master_widgetkitApp.swift
+//  master.widgetkit
+//
+//  Created by Pranav Masekar on 23/07/25.
+//
+
+import SwiftUI
+
+@main
+struct master_widgetkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
